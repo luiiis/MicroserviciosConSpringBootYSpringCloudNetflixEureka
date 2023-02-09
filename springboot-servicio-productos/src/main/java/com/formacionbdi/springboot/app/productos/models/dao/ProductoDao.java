368a080ccd71ddf6f0a2931c40f388fef93573ba
luis <lgarciiaruiiz@gmail.com>
2023-02-10 00:39:47 +0100

    se crea el componente service para los productos

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/dao/ProductoDao.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/dao/ProductoDao.java
@@ -1,5 +1,8 @@
 package com.formacionbdi.springboot.app.productos.models.dao;
 
-public interface ProductoDao {
+import org.springframework.data.repository.CrudRepository;
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+
+public interface ProductoDao extends CrudRepository<Producto,Long>{
 
 }
