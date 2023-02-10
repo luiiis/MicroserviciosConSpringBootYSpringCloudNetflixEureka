3b864a3dd40a66c4dd30be495a2106f92346a7cc
luis <lgarciiaruiiz@gmail.com>
2023-02-10 01:29:50 +0100

    parte 2 de crear componente service item para consumir API productos

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -2,19 +2,25 @@ package com.formacionbdi.springboot.app.item.models.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import com.formacionbdi.springboot.app.item.models.Item;
 
+@Service
 public class ItemServiceImpl implements ItemService {
+	
+	@Autowired
+	private RestTemplate clienteRest;
 
 	@Override
 	public List<Item> findAll() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Item findById(Long id, Integer cantidad) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
