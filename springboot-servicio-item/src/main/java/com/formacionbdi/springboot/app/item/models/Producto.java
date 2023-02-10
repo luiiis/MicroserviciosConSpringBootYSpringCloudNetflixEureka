56a543dc80fdcdc76c9d7de722b3173f40958d3e
luis <lgarciiaruiiz@gmail.com>
2023-02-10 01:17:08 +0100

    se crea un proyecto microservicio items

---
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -0,0 +1,37 @@
+package com.formacionbdi.springboot.app.item.models;
+
+import java.util.Date;
+
+public class Producto {
+  
+	private Long id;
+	private String nombre;
+	private Double precio;
+	private Date creatAt;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreatAt() {
+		return creatAt;
+	}
+	public void setCreatAt(Date creatAt) {
+		this.creatAt = creatAt;
+	}
+	
+	
+}
