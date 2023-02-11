7d5322df863d5a9eaf53c357859939121b673954
luis <lgarciiaruiiz@gmail.com>
2023-02-11 02:11:06 +0100

    se prueba el balanceo de carga en postman entr item y productos

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -8,6 +8,7 @@ public class Producto {
 	private String nombre;
 	private Double precio;
 	private Date creatAt;
+	private Integer port;
 	public Long getId() {
 		return id;
 	}
@@ -31,6 +32,12 @@ public class Producto {
 	}
 	public void setCreatAt(Date creatAt) {
 		this.creatAt = creatAt;
+	}
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
 	}
 	
 	
--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -21,6 +21,9 @@ public class Producto implements Serializable{
 	@Temporal(TemporalType.DATE)
 	private Date  createAt;
 	
+	@Transient
+	private Integer port;
+	
 	public Long getId() {
 		return id;
 	}
@@ -44,6 +47,12 @@ public class Producto implements Serializable{
 	}
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
 	}
 
 }
