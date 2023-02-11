eb8b6d839028e14a896bc7bd4021b9b14d5364a2
luis <lgarciiaruiiz@gmail.com>
2023-02-11 02:25:30 +0100

    se utiliza Ribbon para el balanceo de carga en RestTemplate

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class ItemController {
 	
 	@Autowired
-	@Qualifier("serviceFeign")
+	@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
 	
 	@GetMapping("/listar")
--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +22,15 @@ public class ProductoController {
 	@Autowired
 	private IProductoService productoservice;
 	
+	@Value("${server.port}")
+	private Integer port; 
+	
+	
 	@GetMapping("/listar")
 	public List<Producto> listar(){
 		return productoservice.findAll().stream().map(producto->{
-			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 		
@@ -33,7 +39,8 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
 		Producto producto=productoservice.findById(id);
-		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
 		return producto;
 	}
 
