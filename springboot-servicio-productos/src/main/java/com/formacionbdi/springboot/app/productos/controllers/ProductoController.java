660338486a22061aa24a0e32c537388f40d29f27
luis <lgarciiaruiiz@gmail.com>
2023-02-16 02:45:41 +0100

    se escala microservicios con puerto dinamico

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+//import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,31 +16,29 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 @RestController
 public class ProductoController {
 	
-	@Autowired
-	private Environment env;
+	//@Autowired
+	//private Environment env;
+	
+	//@Value("${server.port}")
+	//private Integer port;
 	
 	@Autowired
-	private IProductoService productoservice;
-	
-	@Value("${server.port}")
-	private Integer port; 
-	
+	private IProductoService productoService;
 	
 	@GetMapping("/listar")
 	public List<Producto> listar(){
-		return productoservice.findAll().stream().map(producto->{
+		return productoService.findAll().stream().map(producto ->{
 			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 			//producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
-		
 	}
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
-		Producto producto=productoservice.findById(id);
+		Producto producto = productoService.findById(id);
 		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		producto.setPort(port);
+		//producto.setPort(port);
 		return producto;
 	}
 
