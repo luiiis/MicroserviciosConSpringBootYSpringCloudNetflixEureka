f6892ce8d721c4329d29ab8628d4b3ce6dcccf82
luis <lgarciiaruiiz@gmail.com>
2023-02-23 19:35:40 +0100

    asunto con el fallbackMethod usando anotaciones @CircuitBreaker y @TimeLimiter

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -56,7 +56,7 @@ public class ItemController {
 	}
 	
 	@CircuitBreaker(name="items",fallbackMethod="metodoAlternativo2")
-	@TimeLimiter(name="items",fallbackMethod="metodoAlternativo2")
+	@TimeLimiter(name="items")
 	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
 	public CompletableFuture<Item>  detalle3(@PathVariable Long id,@PathVariable Integer cantidad) {
 		return CompletableFuture.supplyAsync(()->itemService.findById(id, cantidad));
